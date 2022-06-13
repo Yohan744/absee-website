0049c0fd5611664b5f2059ee4a6fb0e47b6c9ab8
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2022-06-13 13:42:33 +0200

    adding responsive about menu

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,11 @@
 const menuWrapper = document.querySelector('.menu-wrapper')
 const menu = document.querySelector('#menu')
+const body = document.querySelector('body')
 
 menuWrapper.onclick = () => {
     menuWrapper.classList.toggle('active')
     menu.classList.toggle('active')
+    body.classList.toggle('activeMenu')
 }
 
 const menuText = document.querySelectorAll('.menuText')
@@ -11,9 +13,9 @@ const menuText = document.querySelectorAll('.menuText')
 Array.from(menuText).forEach(text => {
 
     text.addEventListener("click", function() {
-        console.log("click")
         menuWrapper.classList.remove('active')
         menu.classList.remove('active')
+        body.classList.remove('activeMenu')
     })
 
 })
