aea2b2054b9dbd5bd3f5aa36ef27ebe751d66429
Yohan744 <73723399+Yohan744@users.noreply.github.com>
2022-06-15 10:06:08 +0200

    adding animation on first section, changing background and text

--- asset/js/main.js
+++ asset/js/main.js
@@ -19,3 +19,53 @@ Array.from(menuText).forEach(text => {
     })
 
 })
+
+
+function changeBackground() {
+
+    const firstSectionBackground = document.querySelector('#firstSectionBackground'),
+        style = window.getComputedStyle(firstSectionBackground),
+        background = style.getPropertyValue('background-color')
+
+    const textHome = document.querySelector(".underline")
+
+    if (background === "rgb(29, 90, 252)") {
+        setTimeout(() => {
+            console.log("orange")
+            textHome.classList.toggle('transition')
+            setTimeout(() => {
+                firstSectionBackground.style.backgroundColor = "rgb(253, 68, 7)"
+                textHome.innerHTML = `de chez soi`
+            }, 300)
+            changeBackground()
+        }, 3000)
+    }
+
+    if (background === "rgb(253, 68, 7)") {
+        setTimeout(() => {
+            console.log("pink")
+            textHome.classList.toggle('transition')
+            setTimeout(() => {
+                firstSectionBackground.style.backgroundColor = "rgb(255, 127, 223)"
+                textHome.innerHTML = `en autonomie`
+            }, 300)
+            changeBackground()
+        }, 3000)
+    }
+
+    if (background === "rgb(255, 127, 223)") {
+        setTimeout(() => {
+            console.log("blue")
+            textHome.classList.toggle('transition')
+            setTimeout(() => {
+                firstSectionBackground.style.backgroundColor = "rgb(29, 90, 252)"
+                textHome.innerHTML = `à son rythme`
+            }, 300)
+            changeBackground()
+        }, 3000)
+    }
+
+}
+
+changeBackground()
+
